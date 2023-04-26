912f951fd8e2e1c61accb7f43173f7877a239ee3
Maciej Adamski <maciej.adamski@ocean.ibm.com>
2023-04-26 21:27:40 +0200

    feat(Brand.tsx): add Brand component with gradient text styling

---
+++ src/components/Brand.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Brand() {
+    return (
+        <h1 className="text-4xl font-bold text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text">
+            Summarizer AI
+        </h1>
+    );
+}
